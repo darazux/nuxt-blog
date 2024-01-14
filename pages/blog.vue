88aa78e2e63d1b8b735189a8f484f3aadc91dc2c
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-14 16:35:21 +0900

    * add @nuxt/image@rc package. * add image in public/images. * add displaying image in blog page.

--- pages/blog.vue
+++ pages/blog.vue
@@ -2,11 +2,19 @@
 
 <template>
   <div>
-    <div v-for="singleData in data" :key="singleData.id">
-      {{ singleData.title }}
-      <br />
-      {{ singleData.date }}
-      <hr />
+    <div>
+      <h1>Blog</h1>
+      <p>エンジニアの日常生活をお届けします</p>
+      <div v-for="singleData in data" :key="singleData.id">
+        <div>
+          <h3>{{ singleData.title }}</h3>
+          <p>{{ singleData.excerpt }}</p>
+          <p>{{ singleData.date }}</p>
+        </div>
+        <div>
+          <nuxt-img :src="singleData.image" alt="blog-image" format="webp" />
+        </div>
+      </div>
     </div>
   </div>
 </template>

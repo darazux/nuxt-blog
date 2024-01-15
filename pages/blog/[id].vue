053315a2c92e7cabed9cd9b1d7a56d0a82214fc2
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-15 16:07:02 +0900

    * add blog/[id].vue for blog single page.

---
+++ pages/blog/[id].vue
@@ -0,0 +1,23 @@
+<!-- [id].vue -->
+
+<template>
+  <h1>記事ページ</h1>
+  <div>
+    <div>
+      <nuxt-img :src="data.image" alt="blog-image" format="webp" />
+    </div>
+    <div>
+      <div>
+        <h1>{{ data.title }}</h1>
+        <p>{{ data.date }}</p>
+        <ContentDoc />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData(useRoute().path, () =>
+  queryContent(useRoute().path).findOne(),
+);
+</script>

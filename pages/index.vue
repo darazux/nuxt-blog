557569074b92bce9bc96b231bdc5fffdd9b071d6
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-15 16:17:59 +0900

    * fix index page content.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,47 @@
 <!-- index.vue -->
 
 <template>
-  <h1>Indexページ</h1>
+  <div>
+    <div>
+      <nuxt-img src="/images/index-hero.jpg" alt="hero" format="webp" />
+      <div>
+        <h1>I'm darazux</h1>
+        <h3>JavaScript Developer</h3>
+      </div>
+    </div>
+    <div>
+      <div>
+        <div>
+          <h2>JavaScript Nerd</h2>
+          <p></p>
+        </div>
+        <nuxt-img src="/images/profile.jpg" alt="profile" format="webp" />
+      </div>
+      <div>
+        <h2>Skills</h2>
+        <div>
+          <div>
+            <img src="/images/javascript.svg" alt="javascript" /><span
+              >JavaScript / 3 years</span
+            >
+          </div>
+          <div>
+            <img src="/images/react.svg" alt="react" />
+            <span>React / 0.5 years</span>
+          </div>
+          <div>
+            <img src="/images/vue.svg" alt="vue" />
+            <span>Vue / 0.5 years</span>
+          </div>
+          <div>
+            <img src="/images/nuxt.svg" alt="nuxt" />
+            <span>Nuxt / 0.5 years</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <NuxtLink to="/contact">Make It Happen!</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>

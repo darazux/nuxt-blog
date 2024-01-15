cefc7b62f5a11dbaa4e61cf1a0f7e2ac9817f4e8
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-15 18:06:52 +0900

    * add prev/next link in blog page.

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,7 +1,6 @@
 <!-- [id].vue -->
 
 <template>
-  <h1>記事ページ</h1>
   <div>
     <div class="hero">
       <nuxt-img :src="data.image" alt="blog-image" format="webp" />
@@ -12,6 +11,7 @@
         <p>{{ data.date }}</p>
         <ContentDoc />
       </div>
+      <PrevNext :prev="prev" :next="next" />
     </div>
   </div>
 </template>
@@ -20,4 +20,8 @@
 const { data } = await useAsyncData(useRoute().path, () =>
   queryContent(useRoute().path).findOne(),
 );
+
+const [prev, next] = await queryContent('/blog')
+  .sort({ id: 1 })
+  .findSurround(useRoute().path);
 </script>
